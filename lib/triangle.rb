988fc4e32d64f2545fdb23854c99dc8b49168371
Alex Lee <hyunho98@gmail.com>
2019-12-23 20:57:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,14 +4,11 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    raise TriangleError
   end
 
   def kind
     if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || @side1 + @side2 < @side3 || @side1 + @side3 < @side2 || @side2 + @side3 < @side1
-      begin
         raise TriangleError
-      rescue TriangleError
       end
     elsif @side1 == @side2 && @side2 == @side3
       return :equilateral
