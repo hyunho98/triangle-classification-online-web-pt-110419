6ecb1b9d75ec62ee20f964f3c87c5f7e679c5670
Alex Lee <hyunho98@gmail.com>
2019-12-23 20:47:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,14 @@ class Triangle
       rescue TriangleError => error
         puts error.message
       end
+    elsif @side1 == @side2 && @side2 == @side3
+      return :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      return :isosceles
+    else
+      return :scalene
+    end
+
   end
 
   class TriangleError < StandardError
